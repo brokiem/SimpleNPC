9b340d46327340306c954f4cbddddceff7c89867
brokiem <milyhqvideo@gmail.com>
2021-06-09 07:53:53 +0800

    updated protocol and added new npcs

---
+++ src/brokiem/snpc/entity/npc/AxolotlNPC.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace brokiem\snpc\entity\npc;
+
+use brokiem\snpc\entity\BaseNPC;
+
+class AxolotlNPC extends BaseNPC {
+
+    public const SNPC_ENTITY_ID = "minecraft:axolotl";
+
+    public $height = 0.4;
+    public $width = 0.3;
+}
