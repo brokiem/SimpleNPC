4b37cb97008573f592212efe18301be1291b107b
Oğuzhan <64753457+OguzhanUmutlu@users.noreply.github.com>
2021-07-27 14:00:11 +0300

    Create EnderDragonNPC.php

---
+++ src/brokiem/snpc/entity/npc/EnderDragonNPC.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Copyright (c) 2021 brokiem
+ * SimpleNPC is licensed under the GNU Lesser General Public License v3.0
+ */
+
+declare(strict_types=1);
+
+namespace brokiem\snpc\entity\npc;
+
+use brokiem\snpc\entity\BaseNPC;
+
+class EnderDragonNPC extends BaseNPC {
+
+    public const SNPC_ENTITY_ID = "minecraft:ender_dragon";
+
+    public $width = 8.0;
+    public $height = 4.0;
+}
